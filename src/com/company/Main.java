b2158eff4e91a870418c7b82665460817b1c9ba5
Anne-Thea <annethearendtorff@gmail.com>
2021-09-22 12:46:59 +0200

    Testing GitHub Upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Point a = new Point(10,5);
         System.out.println(a.toString());
-	// write your code here
+	    // This is a github TEST
     }
 }
