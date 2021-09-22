f4be3bffd123d48f25cd557c99c6739ab12d779d
WeiselO <90724297+WeiselO@users.noreply.github.com>
2021-09-22 14:01:49 +0200

    Created Point variables in Triangle

--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -2,8 +2,15 @@ package com.company;
 import java.awt.*;
 // Oliver arbejder på denne her del af opgaven, og de tilhørende tests
 public class Triangle extends Shape {
-
+    Point pointA;
+    Point pointB;
+    Point pointC;
+    Point middlePoint;
     public Point getCenter(){
         return null;
     }
+    public void computeArea() {}
+    public void computeCirc() {}
+    public void computeDist() {}
+    public void includesPoint() {}
 }
