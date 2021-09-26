e8e4ab379868bdf5361cc8ba5c11ebdfd31bb7e9
Anne-Thea <annethearendtorff@gmail.com>
2021-09-26 20:32:12 +0200

    Making tests for includesPoint method for the class Circle. Improving getCenterTest.

--- Test/com/company/CircleTest.java
+++ Test/com/company/CircleTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class CircleTest {
     Circle circle = new Circle(5,5,10);
     Circle a = new Circle(2,3,15);
+    Point inCircle = new Point(6,8);
+    Point notInCircle = new Point(12,22);
 
     @org.junit.jupiter.api.Test
     void getCenterTest() {
         Point middle = new Point (5,5);
-        assertTrue(circle.getCenter().equals(middle));
+        assertEquals(circle.getCenter(),middle);
     }
 
     @org.junit.jupiter.api.Test
@@ -27,5 +29,15 @@ class CircleTest {
     @org.junit.jupiter.api.Test
     void computeDistToTest(){
         assertEquals(circle.computeDistTo(a), Math.sqrt(13));
+    }
+
+    @org.junit.jupiter.api.Test
+    void includesPointTest1(){
+        assertTrue(circle.includesPoint(inCircle));
+    }
+
+    @org.junit.jupiter.api.Test
+    void includesPointTest2(){
+        assertFalse(circle.includesPoint(notInCircle));
     }
 }
