b4ed7db26602d8698306175ffb4cc08e28d93da0
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2021-09-27 10:05:23 +0200

    Calculation of a triangles center, getCenter, testing of getCenter, and calculation of triangle-sides is implementet. Current error in loop 1 for arbitrary triangle-coordinates (ArrayOutOfBounds). Right triangle-coordinates work.

--- Test/com/company/TriangleTest.java
+++ Test/com/company/TriangleTest.java
@@ -1,19 +1,64 @@
 package com.company;
 
 import org.junit.jupiter.api.Test;
-
+import java.awt.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 
 class TriangleTest {
-
-    @Test
-    void getCenterTest() {
-    }
-    @org.junit.jupiter.api.Test
-    void computeAreaTest() {
-    }
+    Triangle triangle1 = new Triangle(1,2,3,4,5,0);
+    Triangle rightTriangleTest = new Triangle(2,4,-1,0,2,0);
+    int[] rightTriangleSides = {5, 3,4};
+    int [] test;
 
     @org.junit.jupiter.api.Test
-    void computeCircTest() {
+    void getCenterTest1() {
+        Point center1 = new Point(3,2);
+        assertEquals(triangle1.getCenter(),center1);
+        //System.out.println(triangle1.getCenter());
+    }
+
+    @org.junit.jupiter.api.Test
+    void triangleTypeCheckTest(){
+        assertEquals(rightTriangleTest.triangleTypeCheck(), rightTriangleSides);
+        System.out.println(rightTriangleTest.triangleTypeCheck());
+    }
+
+    /*
+    @org.junit.jupiter.api.Test
+    void herePrebTest() {
+
+        test = rightTriangleTest.findTriangleSides(rightTriangleTest.vertexA,
+                rightTriangleTest.vertexB, rightTriangleTest.vertexC);
+
+        System.out.println(test.toString());
+        System.out.print(rightTriangleSides.toString());
+
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void findTriangleSidesTest(){
+        assertEquals(rightTriangleTest.findTriangleSides
+                (this.rightTriangleTest.vertexA, this.rightTriangleTest.vertexB, this.rightTriangleTest.vertexC)
+                ,rightTriangleSides);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void triangleTypeCheckTest(){
+        assertTrue(rightTriangleTest.triangleTypeCheck(rightTriangleTest.findTriangleSides
+                (this.rightTriangleTest.vertexA, this.rightTriangleTest.vertexB, this.rightTriangleTest.vertexC)));
+    }
+    */
+
+    @org.junit.jupiter.api.Test
+    void computeAreaTest1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void computeCircTest1() {
     }
 }
--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -1,16 +1,176 @@
 package com.company;
 import java.awt.*;
-// Oliver arbejder på denne her del af opgaven, og de tilhørende tests
+/*The Math in this assignment is SHHHHIIIIITTTTT. I officially hate triangles!
+ARRRGGGGGGHHHHH,the sheer frustration!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+
 public class Triangle extends Shape {
-    Point pointA;
-    Point pointB;
-    Point pointC;
-    Point center;
-    public Point getCenter(){
-        return null;
+    Point vertexA; //vertex means 'hjørne' in danish
+    Point vertexB;
+    Point vertexC;
+    Point centroid; //fancy (correct) geometric designation for the center of a triangle
+    boolean rightTriangle = false; //If boolean remains false, then triangle is arbitrary
+
+    //Constructor
+    public Triangle(Point a, Point b, Point c) {
+        this.vertexA = a;
+        this.vertexB = b;
+        this.vertexC = c;
     }
+
+    //Overloading to specify coordinates (not really needed, but used for ease of point input)
+    public Triangle(int x1, int y1, int x2, int y2, int x3, int y3){
+        this.vertexA = new Point(x1,y1);
+        this.vertexB = new Point(x2,y2);
+        this.vertexC = new Point(x3,y3);
+    }
+
+    public Point calculateCenter(Point a, Point b, Point c){
+        //Using the build.in Point-getter-function to get x and y for the vertex-coordinates
+        double x1 = a.getX();
+        double y1 = a.getY();
+        double x2 = b.getX();
+        double y2 = b.getY();
+        double x3 = c.getX();
+        double y3 = c.getY();
+
+        //Calculating center by using 'Centroid Formula' --> https://www.cuemath.com/centroid-formula/
+        int cX = (int) (x1 + x2 + x3)/3; //Note that we are forced to use typecasting here,
+        int cY = (int) (y1 + y2 + y3)/3; //because the getX/getY method returns a double
+        centroid = new Point(cX,cY);
+        return centroid;
+    }
+
+    public Point getCenter() {
+        centroid = calculateCenter(this.vertexA, this.vertexB, this.vertexC);
+        return centroid;
+    }
+
+    public int[] findTriangleSides(){
+        Point a = this.vertexA;
+        Point b = this.vertexB;
+        Point c = this.vertexC;
+        double x1 = a.getX();
+        double y1 = a.getY();
+        double x2 = b.getX();
+        double y2 = b.getY();
+        double x3 = c.getX();
+        double y3 = c.getY();
+
+
+        //Sides are calculated by finding the distance between the vertices
+        int sideAB = (int) Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2)); //typecasting
+        int sideBC = (int) Math.sqrt(Math.pow(x3-x2,2)+Math.pow(y3-y2,2)); //typecasting
+        int sideCA = (int) Math.sqrt(Math.pow(x1-x3,2)+Math.pow(y1-y3,2)); //typecasting
+
+        //Insert new values into the array to be returned
+        int[] triangleSides = {sideAB, sideBC, sideCA};
+
+
+        return triangleSides;
+    }
+
+    public boolean triangleTypeCheck(){
+        int sides[] = findTriangleSides();
+        int sideAB = sides[0];
+        int sideBC = sides[1];
+        int sideCA = sides[2];
+        boolean AB = false;
+        boolean BC = false;
+        boolean CA = false;
+
+        //finding out which side of the triangle would be the hypotenuse if it were right-angled
+        loop1: //labelling loop for later break statement
+        for(int i = 0; i <= sides.length; i++){
+            if(sides[i] == sides[0]){
+                sideAB = sides[0];
+                if(sides[0] > sides[1] && sides[0] > sides[2]){
+                    AB = true;
+                    break loop1;
+                }
+            }else if(sides[i] == sides[1]){
+                sideBC = sides[1];
+                if(sides[1] > sides[0] && sides[1] > sides[2]){
+                    BC = true;
+                    break loop1;
+                }
+
+            }else if(sides[i] == sides[2]){
+                sideCA = sides[2];
+                if(sides[2] > sides[0] && sides[2] > sides[1]){
+                    CA = true;
+                    break loop1;
+                }
+            }else{
+                System.out.println("ERROR: hypotenuse cannot be determined");
+            }
+
+        }
+
+        //Confirm whether triangle is right-angled or not using Pythagoras' theorem
+        double ABCheck = Math.pow(sideAB,2);
+        double BCCheck = Math.pow(sideBC,2);
+        double CACheck = Math.pow(sideCA,2);
+
+        if(AB){
+            if(ABCheck == BCCheck + CACheck){
+                return rightTriangle = true;
+            }else{
+                return rightTriangle = false;
+            }
+
+        }else if(BC){
+            if(BCCheck == ABCheck + CACheck){
+                return rightTriangle = true;
+            }else{
+                return rightTriangle = false;
+            }
+
+        }else if(CA){
+            if(CACheck == ABCheck + BCCheck){
+                return rightTriangle = true;
+            }else{
+                return rightTriangle = false;
+            }
+
+        }else{
+            System.out.println("ERROR: Whether triangle is right-angled or not cannot be determined");
+        }
+
+
+        return rightTriangle;
+    }
+
     public double computeArea(){
-        return 0;
+        double area = 0;
+        int triangleSides[] = findTriangleSides();
+        int sideAB = triangleSides[0];
+        int sideBC = triangleSides[1];
+        int sideCA = triangleSides[2];
+
+        boolean AB = true; //test value - have to be fixed to get real value
+        boolean BC = true; //test value - have to be fixed to get real value
+        boolean CA = true; //test value - have to be fixed to get real value
+
+
+        if(rightTriangle){
+            if(AB){
+                area = 1/2 * sideBC * sideCA;
+
+            }else if(BC){
+                area = 1/2 * sideAB * sideCA;
+
+            }else if(CA){
+                area = 1/2 * sideAB * sideBC;
+
+            }else{
+                System.out.println("ERROR");
+            }
+
+        }else{
+
+        }
+
+        return area;
     }
     public double computeCirc(){
         return 0;
@@ -18,4 +178,6 @@ public class Triangle extends Shape {
     public boolean includesPoint(Point a){
         return true;
     }
+
+
 }
