bad67196cc877d645a4d906dacb1bb7db4fadaeb
Anne-Thea <annethearendtorff@gmail.com>
2021-09-28 13:23:01 +0200

    Main test for triangles

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,10 @@ public class Main {
         Point a = new Point(10,5);
         System.out.println("NO ONE HAS FUCKED THIS UP YET");
         //All good in the hood
+        Circle circle = new Circle(3,3,10);
+        Triangle triangle = new Triangle(2,2,1,1,3,3);
+        double dist = circle.computeDistTo(triangle);
+        System.out.println(dist);
     }
 
 }
