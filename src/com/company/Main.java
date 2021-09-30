ddf2a43941057bb35e54f66fb882f4040cc0fbe7
ButItDoesRun <90749391+ButItDoesRun@users.noreply.github.com>
2021-09-30 16:54:47 +0200

    deleted comment in main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         Point a = new Point(10,5);
-        System.out.println("NO ONE HAS FUCKED THIS UP YET");
-        //All good in the hood
         Circle circle = new Circle(3,3,10);
         Triangle triangle = new Triangle(2,2,1,1,3,8);
         Triangle triangle2 = new Triangle(4,4,5,5,8,8);
@@ -15,8 +13,8 @@ public class Main {
         Point z = triangle.getCenter();
         System.out.println(z);
 
-        double areeee = TriangleTest.computeArea();
-        System.out.println(areeee);
+        double area1 = TriangleTest.computeArea();
+        System.out.println(area1);
 
         double dist0 = TriangleTest.computeDistTo(circle);
         System.out.println(dist0);
